61ceef00ebb52943d065a1dd9a80166f2cc0c994
Kendall <moelleke1@gmail.com>
2021-07-20 14:05:33 -0400

    I added buttons and a new viewController

---
+++ final project/opprotunitesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  opprotunitesViewController.swift
+//  final project
+//
+//  Created by Kendall Moeller on 7/20/21.
+//
+
+import UIKit
+
+class opprotunitesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
