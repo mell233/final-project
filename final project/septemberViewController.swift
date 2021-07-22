f8beb5701bad01816f62416625fd97ed792a9e18
AT1512973 <anyatyagi03@gmail.com>
2021-07-22 14:59:32 -0400

    Attached buttons and actions to September View Controller

--- final project/septemberViewController.swift
+++ final project/septemberViewController.swift
@@ -21,6 +21,60 @@ class septemberViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    @IBAction func septemberAlertFour(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+                "RSVP for the Bill Baggs State Park Weekly Cleanup at 9:00 am on September 4th here: https://www.volunteercleanup.org/billbaggscapefloridastatepark/weekly_beach_cleanup_20210904", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+            self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func septemberAlertTen(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+                "Help clean up Montrose Dog Beach at 9:00 am on September 10th by registering here: https://adopt.greatlakes.org/s/cleanup/a2o1L000000huM7QAI/aab19801", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+            self.present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func septemberAlertEleven(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+               "Sign up for the 8:00 am Fort Lauderdale Beach Sweep on September 11th here: https://www.volunteercleanup.org/17509/fort_lauderdale_beach_sweep_20210911", preferredStyle: .alert)
+           alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+           self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func septemberAlertFourteen(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+                "Help clean up the ocean on September 14th at 6:00 pm. For more details go here: https://www.pacificbeachcoalition.org/calendar-2021/!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+            self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func septemberAlertEighteen(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+                "You can help save the earth at 9:00 am on September 18th by participating in the Champion Zone Fitness Beach Clean Up. Sign up here: https://www.volunteercleanup.org/33292/champion_zone_fitness_beach_clean_up", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+            self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func septemberAlertTwentyFive(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+                "Make the world a better place and help clean up Edgewater Beach on September 25th at 10:00 am. You can register here: https://adopt.greatlakes.org/s/cleanupregistration?c__recId=a2o1L000000xWnmQAE ", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+            self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func septemberAlertTwentySix(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+               "At 10:00 am on September 26th you can help restore the beach. Go here for more information: https://www.pacificbeachcoalition.org/calendar-2021/", preferredStyle: .alert)
+           alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+           self.present(alertController, animated: true, completion: nil)
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
