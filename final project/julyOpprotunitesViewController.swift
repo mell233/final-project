ac01cf53ea849c3506ff2d1a1c240c7161a60dd9
Mary <leemary1031@gmail.com>
2021-07-22 14:43:29 -0400

    fix july button and created sept view controller

--- final project/julyOpprotunitesViewController.swift
+++ final project/julyOpprotunitesViewController.swift
@@ -26,7 +26,8 @@ class opprotunitesViewController: UIViewController {
     }
     */
 
-    @IBAction func julyTwentyThirdOpprotunity(_ sender: Any) {
+
+    @IBAction func july23(_ sender: Any) {
         let alertController = UIAlertController(title: "iOScreator", message:
                 "date: July 23, location: South Shore Park Beach in Milwaukee, WI, website: https://adopt.greatlakes.org/s/cleanupregistration?c__recId=a2o1L000000huJDQAY", preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
